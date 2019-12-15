6c7274a53b18defa7f2633ed3c90ff7d938c4dc4
tapoji <tapoji.0710@gmail.com>
2019-12-15 05:22:21 +0000

    test

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -12,12 +12,16 @@ class LinebotController < ApplicationController
 		}
 	end
 
-    def search(id)
-        data = [
-            { "id" => "",  "val" => {"a" =>"屋内", "b" => "屋外" }}, 
-            { "id" => "a", "val" => "http://" }
-        ]
-    	return data.find{|elem| elem["id"] == id }
+    def search id
+        [
+            { "id" => "",  "val" => {"a" => "屋内", "b" => "屋外"} },
+            { "id" => "a",  "val" => {"c" => "音楽", "d" => "運動"} }, #a:屋内
+            { "id" => "c",  "val" => {"e" => "歌", "f" => "楽器"} }, #c:音楽
+            { "id" => "e",  "val" => "https://karaokeclub.jp/shop/%E3%82%AB%E3%83%A9%E3%82%AA%E3%82%B1club-dam-resort-%E6%9D%BE%E6%B1%9F%E9%A7%85%E5%89%8D%E9%80%9A%E3%82%8A%E5%BA%97/" }, #e:歌
+            { "id" => "f",  "val" => "https://gakkiya-navi.com/shimane/matsue/320003/" }, #f:楽器
+            { "id" => "d",  "val" => "https://t2style.jp/" }, #d:運動
+            { "id" => "b",  "val" => "https://jmty.jp/shimane/com-kw-%E7%99%BB%E5%B1%B1" } #b:屋外
+        ].find{|e| e["id"] == id }
     end
 
 
